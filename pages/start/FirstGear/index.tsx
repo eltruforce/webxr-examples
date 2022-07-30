107c06b3d95402a11d92ead1c9e24b6bf1be09da
renatmc <lordace550@gmail.com>
2022-07-29 21:48:12 -0500

    feat: add 3 ways of initial setups to first gear

--- pages/start/FirstGear/index.tsx
+++ pages/start/FirstGear/index.tsx
@@ -1,23 +1,59 @@
-import { Component } from "react";
+import { Component, useEffect } from "react";
 
+// Way 1
+// -------------------------------------------------------------------------------------------------
 export default class App extends Component {
-  componentDidMount(): void {
+  componentDidMount = () => {
     const container = document.createElement("div");
     document.body.appendChild(container);
 
-    window.addEventListener("resize", resize.bind(this));
+    window.addEventListener("resize", this.resize.bind(this));
 
-    function resize() {
+  };
 
-    }
-    resize();
+  resize = () => {};
 
-    function render() {
-        
-    }
-    render();
-  }
-  render() {
-    return <div id="container" />;
-  }
+  animate = () => {};
+
+  render = () => <div id="container" />;
 }
+// -------------------------------------------------------------------------------------------------
+
+// Way 2
+// -------------------------------------------------------------------------------------------------
+// export default class App extends Component {
+//   componentDidMount(): void {
+//     const container = document.createElement("div");
+//     document.body.appendChild(container);
+
+//     window.addEventListener("resize", resize.bind(this));
+
+//     function resize() {}
+
+//     function render() {}
+//   }
+//   render() {
+//     return <div id="container" />;
+//   }
+// }
+// -------------------------------------------------------------------------------------------------
+
+// Way 3
+// -------------------------------------------------------------------------------------------------
+// function App() {
+//   useEffect(() => {
+//     const container = document.createElement("div");
+//     document.body.appendChild(container);
+
+//     window.addEventListener("resize", resize.bind(this));
+
+//     function resize() {}
+
+//     function animate() {}
+//   }, []);
+
+//   return <div id="container" />;
+// }
+
+// export default App;
+// -------------------------------------------------------------------------------------------------
