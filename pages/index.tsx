3a556984682d87c8b7d0ffb67e4c23ee56c559bc
renatmc <lordace550@gmail.com>
2022-07-30 22:11:37 -0500

    feat: add gear menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,32 @@
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      <h5>WebXR examples links</h5>
+      <ul>
+        <li>
+          First Gear <a href="start/FirstGear">go!</a>
+        </li>
+        <li>
+          Second Gear <a href="start/SecondGear">go!</a>
+        </li>
+        <li>
+          Third Gear <a href="start/ThirdGear">go!</a>
+        </li>
+        <li>
+          Fourth Gear <a href="start/FourthGear">go!</a>
+        </li>
+        <li>
+          Fifth Gear <a href="start/FifthGear">go!</a>
+        </li>
+        <li>
+          Sixth Gear <a href="start/SixthGear">go!</a>
+        </li>
+        <li>
+          Seventh Gear <a href="start/SeventhGear">go!</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default HomePage;
