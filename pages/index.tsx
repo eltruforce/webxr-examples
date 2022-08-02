c72a5c0c27b1cc14f1e7f051600297d44647395a
renatmc <lordace550@gmail.com>
2022-08-02 10:01:31 -0500

    feat: add seventh-gear

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,25 +4,36 @@ function HomePage() {
       <h5>WebXR examples links</h5>
       <ul>
         <li>
-          First Gear <a href="start/FirstGear">go!</a>
+          First Gear&emsp; <a href="three/FirstGear">go three!</a>&emsp;
+          <a href="react-three-fiber/FirstGear">go react-three-fiber!</a>
         </li>
         <li>
-          Second Gear <a href="start/SecondGear">go!</a>
+          Second Gear&emsp; <a href="three/SecondGear">go three!</a>&emsp;
+          <a href="react-three-fiber/SecondGear">go react-three-fiber!</a>
         </li>
         <li>
-          Third Gear <a href="start/ThirdGear">go!</a>
+          Third Gear&emsp; <a href="three/ThirdGear">go three!</a>&emsp;
+          <a href="react-three-fiber/ThirdGear">go react-three-fiber!</a>
         </li>
         <li>
-          Fourth Gear <a href="start/FourthGear">go!</a>
+          Fourth Gear&emsp; <a href="three/FourthGear">go three!</a>&emsp;
+          <a href="react-three-fiber/FourthGear">go react-three-fiber!</a>
         </li>
         <li>
-          Fifth Gear <a href="start/FifthGear">go!</a>
+          Fifth Gear&emsp; <a href="three/FifthGear">go three!</a>&emsp;
+          <a href="react-three-fiber/FifthGear">go react-three-fiber!</a>
         </li>
         <li>
-          Sixth Gear <a href="start/SixthGear">go!</a>
+          Sixth Gear&emsp; <a href="three/SixthGear">go three!</a>&emsp;
+          <a href="react-three-fiber/SixthGear">go react-three-fiber!</a>
         </li>
         <li>
-          Seventh Gear <a href="start/SeventhGear">go!</a>
+          Seventh Gear&emsp; <a href="three/SeventhGear">go three!</a>&emsp;
+          <a href="react-three-fiber/SeventhGear">go react-three-fiber!</a>
+        </li>
+        <li>
+          Eighth Gear&emsp; <a href="three/EighthGear">go three!</a>&emsp;
+          <a href="react-three-fiber/EighthGear">go react-three-fiber!</a>
         </li>
       </ul>
     </div>
