26c9f7eb7d884a270d8044e409c9456f21447d0f
renatmc <lordace550@gmail.com>
2023-02-22 22:31:14 -0500

    fix: box container canvas

--- pages/react-three/FirstGear/index.tsx
+++ pages/react-three/FirstGear/index.tsx
@@ -30,9 +30,12 @@ const App = () => {
       <ContainerBox
         ref={containerRef}
         style={{
-          width: "100vw",
-          height: "100vh",
-          display: "flex",
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          textAlign: "center",
           justifyContent: "center",
           alignItems: "center",
         }}
--- pages/react-three/SecondGear/index.tsx
+++ pages/react-three/SecondGear/index.tsx
@@ -29,9 +29,12 @@ const App = () => {
       <Box
         ref={containerRef}
         style={{
-          width: "100vw",
-          height: "100vh",
-          display: "flex",
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          textAlign: "center",
           justifyContent: "center",
           alignItems: "center",
         }}
